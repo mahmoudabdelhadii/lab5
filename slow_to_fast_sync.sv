e7b04d30eaf896ff80c4e9b89c7baf8c92f12229
rominaadib <57119684+rominaadib@users.noreply.github.com>
2021-03-30 21:48:32 -0600

    fixed some oops, now we have slow-fast sync

--- slow_to_fast_sync.sv
+++ slow_to_fast_sync.sv
@@ -1,28 +1,29 @@
-module fast_to_slow_sync (fast_clk, slow_clk, d, q);
+module slow_to_fast (slow_clk, fast_clk, d, q);
 	parameter N = 12;
-	input fast_clk, slow_clk;
+	input slow_clk, fast_clk;
 	input [N-1:0] d;
 	output [N-1:0] q;
 
-	logic [N-1:0] q1, q1, q3, E;
+	logic [N-1:0]] q1, q2, q3, E;
 
-	always_ff @ (posedge  fast_clk) begin
+	always_ff @ (posedge slow_clk) begin
 		q1 <= d;
 	end
-	
+
 	always_ff @ (posedge fast_clk or posedge E) begin
 		if (E) q2 <= {N{1'b0}};
 		else q2 <= q1;
-	end
-
-	always_ff @ (posedge slow_clk) begin
-		q <= q2;
 	end
 
 	always_ff @ (posedge ~fast_clk) begin
 		q3 <= slow_clk;
 		E <= q3;
 	end
-endmodule
-	
 
+	always_ff @ (posedge fast_clk) begin
+		q <= q2;
+	end
+
+endmodule 
+
+	
